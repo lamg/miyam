df9b9988a850becf0548db9b9421d11c7c9a602f
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2018-12-09 23:17:17 -0500

    interesting experiment with closures

--- miyam.go
+++ miyam.go
@@ -16,10 +16,30 @@ type miyam struct {
 }
 
 // (nc, url)
-type extractor func(*netConf, string) (*info, error)
+type cfgExtr func(*netConf, string) (*info, error)
+
+type extractor func(string) (*info, error)
+
+func clsExtr(nc *netConf, ex cfgExtr) (r extractor) {
+	r = func(url string) (i *info, e error) {
+		i, e = ex(nc, url)
+		return
+	}
+	return
+}
 
 // (nc, url, offset) (rc, e)
-type netRC func(*netConf, string, uint64) (io.ReadCloser, error)
+type cfgNetRC func(*netConf, string, uint64) (io.ReadCloser, error)
+
+type netRC func(string, uint64) (io.ReadCloser, error)
+
+func clsNetRC(nc *netConf, nrc cfgNetRC) (r netRC) {
+	r = func(url string, offset uint64) (rc io.ReadCloser, e error) {
+		rc, e = nrc(nc, url, offset)
+		return
+	}
+	return
+}
 
 // (matcher, s) (offset, w, e)
 type fileWC func(string, *stream) (uint64, io.WriteCloser, error)
@@ -48,25 +68,26 @@ type dwCfg struct {
 	formats []string
 }
 
-func (m *miyam) dwInfo(url string) (inf *info, e error) {
-	for i := 0; e == nil && inf == nil && i != len(m.alts); i++ {
-		inf, e = m.alts[i](m.nc, url)
+func dwInfo(alts []extractor, url string) (inf *info, e error) {
+	for i := 0; e == nil && inf == nil && i != len(alts); i++ {
+		inf, e = alts[i](url)
 	}
 	return
 }
 
-func (m *miyam) dwnStreams(inf *info) (fs []string, e error) {
-	if !m.dc.skip {
-		fs = make([]string, len(m.dc.formats))
-		for i := 0; e == nil && i != len(m.dc.formats); i++ {
-			str := inf.formats[m.dc.formats[i]]
+func dwnStreams(dc *dwCfg, inf *info, fwc fileWC,
+	nrc netRC) (fs []string, e error) {
+	if !dc.skip {
+		fs = make([]string, len(dc.formats))
+		for i := 0; e == nil && i != len(dc.formats); i++ {
+			str := inf.formats[dc.formats[i]]
 			fs[i] = str.file
 			var offset uint64
 			var wc io.WriteCloser
-			offset, wc, e = m.fwc(inf.matcher, &str)
+			offset, wc, e = fwc(inf.matcher, &str)
 			var rc io.ReadCloser
 			if e == nil {
-				rc, e = m.nrc(m.nc, str.url, offset)
+				rc, e = nrc(str.url, offset)
 			}
 			if e == nil {
 				_, e = io.Copy(wc, rc)
@@ -76,7 +97,7 @@ func (m *miyam) dwnStreams(inf *info) (fs []string, e error) {
 	return
 }
 
-func (m *miyam) mux(paths []string) (e error) {
+func mux(paths []string) (e error) {
 	// TODO search Go library for merging the streams
 	// in a Matroska file
 	return
